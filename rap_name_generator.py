9172ed12ea296190e2e741ecee7cf48c409b9e58
Khyta <33177286+Khyta@users.noreply.github.com>
2022-04-11 18:33:09 +0200

    Updated dependencies and version
    Uses the requests library now, goes easier on the API and requests only each 9th time, asks the user if they want to continue generating names as opposed to making a new request every second.

--- rap_name_generator.py
+++ rap_name_generator.py
@@ -1,13 +1,36 @@
-#coding a rap name generator with the starting string:"Lil" + a random word from the urban dictionary API
+# coding a rap name generator with the starting string:"Lil" + a random word from the urban dictionary API
 #import libraries
-import time
-import os
-import urllib
+import requests
+import random
 import json
-while True:
-    url = "http://api.urbandictionary.com/v0/random" #getting the url
-    response = urllib.urlopen(url) #ope the url with urllib
-    data = json.loads(response.read()) #read the API
-    word = data['list'][0]['word']
-    print"Lil", word #print the word
-    time.sleep(1)
+usr = 'y'
+index = 0
+
+url = "http://api.urbandictionary.com/v0/random"  # get the urban dictionary API
+
+
+def fetch_api(url):
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+
+data = fetch_api(url)
+
+while usr == 'y' and index < 10:
+    word = data['list'][index]['word']     # get the word
+    print('------------------------------------------------------')
+    print("Lil", word, '\n')  # print the word
+    # Remove the [ and ] from the definition string
+    definition = data['list'][index]['definition'].replace('[', '').replace(']', '')
+    print("Definition:", definition + '\n')  # print the definition"
+    usr = input("Would you like to generate another name? (y/n) ")
+    index += 1
+    if usr == 'n':
+        print('------------------------------------------------------')
+        print("Thanks for using the lil rap name generator!")
+        break
+    if index == 9:
+        print('One second, getting new names...')
+        data = fetch_api(url)
+        index = 0
